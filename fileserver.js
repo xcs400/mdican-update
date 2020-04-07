d97502403dff67e9828e6ac7864424b49a601541
xcs400 <xcs400.pp@gmail.com>
2020-04-07 17:11:17 +0200

    Update fileserver.js

--- fileserver.js
+++ fileserver.js
@@ -1,20 +1,56 @@
 const { Client } = require('pg');
 
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+
+  
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
 
 console.log ( process.env.DATABASE_URL)
-client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', (req, res) => res.send(showTimes()))
+  
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM logaccesms');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
+  
+  
+
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  
+  showTimes = () => {
+  let result = ''
+  const times = process.env.TIMES || 5
+  for (i = 0; i < times; i++) {
+    result += i + ' '
   }
-  client.end();
-});
+  return result;
+}
+
 
 
 
