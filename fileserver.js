26257db8402513b65ec278529833b43114ca7ec4
xcs400.pp@gmail.com <xcs400.pp@gmail.com>
2020-04-07 16:51:38 +0200

    Update fileserver.js

--- fileserver.js
+++ fileserver.js
@@ -1,5 +1,25 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 
+
+
+
+/*
 
 const http = require('http');
 const url = require('url');
@@ -104,7 +124,7 @@ client.query('INSERT INTO logaccess (nom,dateacces,Stamp) VALUES ("mon nom1 ","2
 
 });
 
-
+/*
 /*
 
 
