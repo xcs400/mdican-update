f42c3d3d75f0b7b57ec311a186e34e3f8c3fb951
xcs400.pp@gmail.com <xcs400.pp@gmail.com>
2020-04-08 09:14:46 +0200

    Update fileserver.js

--- fileserver.js
+++ fileserver.js
@@ -35,11 +35,13 @@ express()
     try {
       const client = await pool.connect()
 
-		
-	 var sql= 'INSERT INTO logaccess (nom ,dateacces,url,version) VALUES ( ' 
+ let date_ob = new Date();
+		  
+	 var sql= 'INSERT INTO logaccess (nom ,dateacces,url,Recorddate,version) VALUES ( ' 
 	 sql =sql+ '\''+  req.query.nom  + '\' ,'
 	 sql =sql+ '\''+  req.query.dateacces  + '\' ,'
 	 sql =sql+ '\''+  req.query.url   + '\' ,'
+	 sql =sql+ '\''+  date_ob   + '\' ,'
 	 sql =sql+ '\''+  req.query.version  + '\' '
 	 	 
 	 sql =sql + ');'
@@ -86,114 +88,6 @@ express()
 
 
 
-
-/*
-
-
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
-
-  
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-  rejectUnauthorized: false
-});
-
-
-   
- 
-// vous pouvez passer le paramètre en ligne de commande. ex. node static_server.js 3000
-var port =  process.env.PORT || 80;
-
-http.createServer(function (req, res) {
-  console.log(`${req.method} ${req.url}`);
-  // découpe l'URL
-  const parsedUrl = url.parse(req.url);
-  // Extrait le chemin de l'URL
-  let pathname = `.${parsedUrl.pathname}`;
-  // Associe le type MIME par rapport au suffixe du fichier demandé
-  const mimeType = {
-    '.ico': 'image/x-icon',
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.md': 'text/html',
-    '.json': 'application/json',
-    '.css': 'text/css',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.wav': 'audio/wav',
-    '.mp3': 'audio/mpeg',
-    '.svg': 'image/svg+xml',
-    '.pdf': 'application/pdf',
-    '.doc': 'application/msword',
-    '.eot': 'appliaction/vnd.ms-fontobject',
-    '.ttf': 'application/font-sfnt'
-  };
-  fs.exists(pathname, function (exist) {
-    if(!exist) {
-      // si le fichier n'existe pas, renvoie 404
-      res.statusCode = 404;
- 	  console.log(process.env.DATABASE_URL)
-   
-   
-     const client = await pool.connect()
-	  
-client.query('INSERT INTO logaccess (nom,dateacces,Stamp) VALUES (\'mon nom1\',\'2020-07-04\',\'2020-07-04\'::timestamp);', (err, res) => {
-	   console.log( " erreur sql");
-	   console.log( res);
-	   console.log( err); 
-
-	});
-	 
-	 
-      const result = await client.query('SELECT * FROM logaccess');
-      const results = { 'results': (result) ? result.rows : null};
-	  res.end ( results)
-
-	 
-      client.release();
-    
-	
-	
-	//   res.end(`File ${pathname} not found!`);
-  	
-
-
-      return;
-    }
-    // s'il s'agit d'un répertoire, on tente d'y trouver un fichier index.html
-    if (fs.statSync(pathname).isDirectory()) {
-      pathname += '/index.html';
-	
-
-    }
-    // lecture du fichier local
-    fs.readFile(pathname, function(err, data){
-      if(err){
-        res.statusCode = 500;
-        res.end(`Error getting the file: ${err}.`);
-      } else {
-        // extraction du suffixe de fichier selon le chemin basé sur l'URL fournie. ex. .js, .doc, ...
-        const ext = path.parse(pathname).ext;
-        // si le fichier est trouvé, définit le content-type et envoie les données
-        res.setHeader('Content-type', mimeType[ext] || 'text/plain' );
-        res.end(data);
-		
-	
-		
-      }
-    });
-  });
-}).listen(parseInt(port));
-console.log(`Le serveur ecoute sur le port ${port}`);
- 
-
-*/
 
 
 
