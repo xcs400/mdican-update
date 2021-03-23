efe7c628ad98b09883cad6d058c38b5d0ec6b340
Pascal Fillon <P.FILLON@tiama.com>
2021-03-23 16:39:36 +0100

    Revert "test"
    This reverts commit 26b9fde789ab42675c29da29fba97207d409e4ac.

--- fileserver.js
+++ fileserver.js
@@ -64,7 +64,7 @@ express()
     try {
       const client = await pool.connect()
 
- 	  
+	  
  let date_ob = new Date();
 
 	 var sql= 'INSERT INTO logaccess (nom ,dateacces,url,Recorddate,version) VALUES ( ' 
