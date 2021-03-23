2079590dfecefbb32ee06c20171df07f9d12cdbc
Pascal Fillon <P.FILLON@tiama.com>
2021-03-23 16:33:06 +0100

    Update fileserver.js

--- fileserver.js
+++ fileserver.js
@@ -12,8 +12,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
    ssl: { rejectUnauthorized: false },
-  rejectUnauthorized: false
-});
+ });
 
 
 
