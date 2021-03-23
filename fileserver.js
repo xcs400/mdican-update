bc3872f89a0b3d441d75a999e82f88089537a73c
Pascal Fillon <P.FILLON@tiama.com>
2021-03-23 16:47:45 +0100

    Update fileserver.js

--- fileserver.js
+++ fileserver.js
@@ -4,25 +4,22 @@ const { Client } = require('pg');
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 5000
 
 
   
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-    };  
-    
-    
- });
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+  rejectUnauthorized: false
+});
 
 
 
 
 
-console.log ( process.env.DATABASE_URL)
+//console.log ( process.env.DATABASE_URL)
 
 
 
